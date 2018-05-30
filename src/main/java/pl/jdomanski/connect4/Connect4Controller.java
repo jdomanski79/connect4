7f7d4923fdc6de2ef437e4df8f3ea4f4044a1ecd
JD <me@codenvy.io>
2018-05-30 12:14:44 +0000

    intial commit

---
+++ src/main/java/pl/jdomanski/connect4/Connect4Controller.java
@@ -0,0 +1,16 @@
+package pl.jdomanski.connect4;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Connect4Controller {
+    
+    @MessageMapping("/hello")
+    @SendTo("/topic/game")
+    public String game(String message){
+        System.out.println("got new message: " + message);
+        return message;
+    }
+}
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,48 @@
+let stompClient = null;
+let count = 0;
+const appUri = "/app/hello";
+const messageParagraph = document.getElementById("message");
+
+document.getElementById("connect").addEventListener("click", ()=>{
+    connect();
+});
+
+document.getElementById("sendHello").addEventListener("click", (event)=>{
+    //console.log("Hello from sendHello");
+    count++;
+    stompClient.send(appUri, {}, "Sending hello " + count);
+});
+
+const cells = document.querySelectorAll(".board-column");
+cells.forEach( cell => {
+    cell.addEventListener("click", (event)=>{ 
+        sendColumn(event.target.dataset.column);
+    })
+  });
+
+// websocket connection
+function connect(){
+    let socket = new SockJS('/game');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame){
+        console.log("Connected!!! " + frame);
+        cd  
+        stompClient.subscribe('/topic/game', function(message) {
+            messageParagraph.innerHTML += "Column clicked :" + message.body +"\n";
+            toggleColumn(message.body);
+        })
+    })
+}
+
+
+
+function sendColumn(column){
+    stompClient.send(appUri, {}, column);
+}
+
+function toggleColumn(column){
+    const col = document.querySelector(`.board-column[data-column="${column}"]`);
+    for( let i = 0; i< col.children.length; i++) {
+        col.children[i].classList.toggle("clicked");
+    }
+}
